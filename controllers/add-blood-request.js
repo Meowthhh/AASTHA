2e0224cbd8e5b9d99ba042df27f0bb025a98e2b2
rozasajneen17 <121815605+rozasajneen17@users.noreply.github.com>
2023-01-02 22:20:54 +0600

    Update add-blood-request.js

--- controllers/add-blood-request.js
+++ controllers/add-blood-request.js
@@ -6,10 +6,6 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var db = require('../config/dbconfig');
 var sweetalert = require('sweetalert2');
-const { check, validationResult } = require('express-validator');
-const jwt = require('jsonwebtoken');
-const bcrypt = require("bcryptjs");
-const { route } = require('./home');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
