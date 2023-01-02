971042d1d5baf9e44c336f4f1ef7b92e418dd94e
rozasajneen17 <121815605+rozasajneen17@users.noreply.github.com>
2023-01-02 22:04:04 +0600

    Update add-blood-request.js

--- controllers/add-blood-request.js
+++ controllers/add-blood-request.js
@@ -6,9 +6,4 @@ router.get('*', function (req, res, next) {
 
     db.query('INSERT INTO add_blood_requst SET ?',{ user_email: user_email,
         pname: pname, c_number: c_number, complications: complicaitons, date: date , b_g : b_g , quantity: quantity, 
-        add_requirement: add_requirement , organization: organization , address: address
-    },function(err,result){
-        if(err)
-        {
-            console.log(err)
-        }
+        add_requirement: add_requirement , organization: organization , address: addres
