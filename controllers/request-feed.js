6138b880fd478cf48c1d92d5f377374f073da7b1
Rakin Abrar <75223578+Rakin126@users.noreply.github.com>
2023-01-03 12:41:15 +0600

    Update request-feed.js

--- controllers/request-feed.js
+++ controllers/request-feed.js
@@ -25,11 +25,6 @@ router.get('/', (req, res) => {
     db.query("SELECT * FROM add_blood_requst", function (error, results) {
         if (error) {
             throw error;
-        }
-        else {
-            console.log(results)
-            res.render('request-feed.ejs', { data: results });
-        }
     });
 });
 
