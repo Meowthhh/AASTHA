54178034e407b9dc7b5a943ae1da5f3512280302
Rakin Abrar <75223578+Rakin126@users.noreply.github.com>
2023-01-03 13:05:25 +0600

    Update dashboard.js

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -8,16 +8,28 @@ var db = require('../config/dbconfig');
 var sweetalert = require('sweetalert2');
 const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs")
 
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
-router.get('*', function (res, next) {
+router.get('*', function (req, res, next) {
+    if (req.cookies['email'] == null) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
 });
 
-router.get(function (req) {
+router.get('/', function (req, res) {
 
     // console.log(req.cookies['email']);
     email = req.cookies['email'];
     console.log(email);
-    db.query([email], async=> {
+    db.query("SELECT * FROM `user_info` where email=?", [email], async (err, result) => {
+        console.log(result);
+        res.render('dashboard.ejs',{username : result[0].fname});
     })
 });
+
+module.exports = router;
