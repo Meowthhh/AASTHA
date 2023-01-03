5bf02367ef858969c89f9f30ceb7d5e39b5ca2d5
Rakin Abrar <75223578+Rakin126@users.noreply.github.com>
2023-01-03 12:40:30 +0600

    Update logout.js

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res){
+
 	//req.session.username = null;
-	//res.clearCookie('email')
+	res.clearCookie('email');
+	res.redirect('/login');
+});
+
+module.exports = router;
